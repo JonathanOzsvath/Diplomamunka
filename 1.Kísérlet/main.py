f591213e4db5b81d1ed6663ecd6e9225e4419db9
JonathanOzsvath <ozsvathjonathan@gmail.com>
2019-10-09 17:53:21 +0200

    Brute Force Matching Hamming távolság alapján (Saját és OpenCV-s)

--- 1.Kísérlet/main.py
+++ 1.Kísérlet/main.py
@@ -2,20 +2,24 @@ import cv2
 import numpy as np
 
 numberOfKeypoint = 500
-hMin = 110
+hMin = 80
 
 
 def bruteForce(keyPoint, description):
     description_bitstring_array = uint8ArrayToBitstring(description)
-    sorted_hamming_distance_array = makeAllHammingDistance(description_bitstring_array)
-    filtered_hamming_distance_array = filterByHammingDistance(sorted_hamming_distance_array)
+    sorted_hamming_distance_array = makeAllHammingDistance(description_bitstring_array, description_bitstring_array)
+    firstArray = [i[0] for i in sorted_hamming_distance_array]
 
-    filtered_indices = getDrawIndices(filtered_hamming_distance_array)
+    filtered_hamming_distance_array = list(filter(lambda m: m[1] > hMin, firstArray))
+
+    filtered_indices = [i[0] for i in filtered_hamming_distance_array]
+    # duplikáció szűrése
+    filtered_indices = sorted(list(dict.fromkeys(filtered_indices)))
     kp2 = getDrawKeypoints(keyPoint, filtered_indices)
 
-    drawKeypoints(img1, kp2, 0.25, 0.25, "Filtered")
+    print(len(kp2))
 
-    cv2.waitKey(0)
+    drawKeypoints(img1, kp2, 0.25, 0.25, "Own Brute Force Filtered")
 
 
 def drawKeypoints(image, keyPoint, fx=1.0, fy=1.0, title='', isGray=True):
@@ -29,6 +33,7 @@ def drawKeypoints(image, keyPoint, fx=1.0, fy=1.0, title='', isGray=True):
 
 
 def getDrawKeypoints(kp, draw_indices):
+    """ Vissza adja a kirajzolandó kulcspontokat."""
     drawKeyPoints = []
     for i in draw_indices:
         drawKeyPoints.append(kp[i])
@@ -36,35 +41,16 @@ def getDrawKeypoints(kp, draw_indices):
     return drawKeyPoints
 
 
-def getDrawIndices(filtered_hamming_distance_array):
-    indices = list(range(0, numberOfKeypoint))
-    copy_filtered_hamming_distance_array = filtered_hamming_distance_array.copy()
-
-    for element in copy_filtered_hamming_distance_array:
-        for e in element:
-            if e[0] in indices:
-                indices.remove(e[0])
-                copy_filtered_hamming_distance_array.remove(filtered_hamming_distance_array[e[0]])
-
-    return indices
-
-
-def filterByHammingDistance(sorted_hamming_distance_array):
-    filtered_hamming_distance_array = []
-    for element in sorted_hamming_distance_array:
-        filtered_hamming_distance_array.append(list(filter(lambda h: h[1] < hMin, element)))
-
-    return filtered_hamming_distance_array
-
-
-def makeAllHammingDistance(description_bitstring_array):
+def makeAllHammingDistance(description_bitstring_array1, description_bitstring_array2):
+    """ Két bitstringeket tartalmazó tömbből az összes variáció szeint hamming távolságot számol.
+        Minden elemnél távolság alapján rendez"""
     hamming_distance_array = []
-    for i in range(0, len(description_bitstring_array)):
+    for i in range(0, len(description_bitstring_array1)):
         hamming_distances = []
-        for j in range(0, len(description_bitstring_array)):
+        for j in range(0, len(description_bitstring_array2)):
             if i != j:
                 hamming_distances.append(
-                    (j, hammingDistance(description_bitstring_array[i], description_bitstring_array[j])))
+                    (j, hammingDistance(description_bitstring_array1[i], description_bitstring_array2[j])))
 
         sorted_hamming_distance = sorted(hamming_distances, key=lambda hd: hd[1])
         hamming_distance_array.append(sorted_hamming_distance)
@@ -73,6 +59,7 @@ def makeAllHammingDistance(description_bitstring_array):
 
 
 def uint8ArrayToBitstring(description):
+    """A kulcspontokpól jövő desc vektort alakítja string tömbbé"""
     description_bitstring = []
     for i in range(0, description.shape[0]):
         string_array = []
@@ -84,25 +71,30 @@ def uint8ArrayToBitstring(description):
 
 
 def hammingDistance(bitstring1, bitstring2):
+    """Két bitstring hamming távolságát adja vissza"""
     counter = 0
     for i in range(0, len(bitstring1)):
-        if bitstring1[i] == bitstring2[i]:
+        if bitstring1[i] != bitstring2[i]:
             counter += 1
 
     return counter
 
 
-def opencvBruteForce(description):
+def opencvBruteForce(keyPoint, description):
     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
     matches = bf.knnMatch(description, description, k=2)
     matches2 = matches.copy()
     for match in matches:
         match.pop(0)
 
-    for match in matches:
-        if match[0].distance < 50:
-            matches2.remove(match)
-    a = 0
+    filtered_hamming_distance_array = list(filter(lambda m: m[0].distance > hMin, matches))
+
+    filtered_indices = [i[0].queryIdx for i in filtered_hamming_distance_array]
+    kp2 = getDrawKeypoints(keyPoint, filtered_indices)
+
+    print(len(kp2))
+
+    drawKeypoints(img1, kp2, 0.25, 0.25, "OpenCV Brute Force Filtered")
 
 
 if __name__ == '__main__':
@@ -112,7 +104,9 @@ if __name__ == '__main__':
     # Initiate ORB
     orb = cv2.ORB_create(numberOfKeypoint)
     kp, des = orb.detectAndCompute(img1, None)
-    # drawKeypoints(img1, kp, 0.25, 0.25, "ORB")
+    drawKeypoints(img1, kp, 0.25, 0.25, "ORB")
 
-    # brute_force(kp, des)
-    opencvBruteForce(des)
+    bruteForce(kp, des)
+    opencvBruteForce(kp, des)
+
+    cv2.waitKey(0)
