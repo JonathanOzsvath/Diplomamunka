2d57a4aa37e8778a1bb2e17b8d358726099a57d9
JonathanOzsvath <ozsvathjonathan@gmail.com>
2019-10-14 21:44:55 +0200

    Kép beolvasása és egérkattintások elmentés {képnév}.click fájlba

---
+++ Homography/click_points.py
@@ -0,0 +1,56 @@
+import cv2
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+mouseClicks = []
+imageName = 'darts1_2'
+deleteSquareSide = 3
+
+
+def showImageWithClicks(radius=3, color=(0, 255, 0)):
+    imgCopy = img.copy()
+    for click in mouseClicks:
+        cv2.circle(imgCopy, click, radius, color, thickness=-1)
+
+    cv2.imshow(imageName, imgCopy)
+
+
+def setMouseCallback(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONUP:
+        mouseClicks.append((x, y))
+        showImageWithClicks()
+
+    elif event == cv2.EVENT_RBUTTONUP:
+        for click in mouseClicks:
+            if calcDistance((x, y), click) < 10:
+                mouseClicks.remove(click)
+                showImageWithClicks()
+
+
+def calcDistance(point1, point2):
+    return math.sqrt((point2[0] - point1[0]) * (point2[0] - point1[0]) + (point2[1] - point1[1]) * (point2[1] - point1[1]))
+
+
+def savePoints():
+    with open(imageName + '.click', 'w') as f:
+        for x, y in mouseClicks:
+            f.write('{}, {}\n'.format(x, y))
+
+
+if __name__ == '__main__':
+    # referencia kép
+    img = cv2.imread('../images/' + imageName + '.jpg')
+    img = cv2.resize(img, None, fx=0.25, fy=0.25, interpolation=cv2.INTER_CUBIC)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+    cv2.imshow(imageName, img)
+
+    cv2.setMouseCallback(imageName, setMouseCallback)
+    k = cv2.waitKey(0)
+
+    if k == 27:
+        cv2.destroyAllWindows()
+    elif k == 13:
+        savePoints()
