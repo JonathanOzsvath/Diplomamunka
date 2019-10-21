cdc796f5ddeb46fb8de59cb331a000f69ebea70b
JonathanOzsvath <ozsvathjonathan@gmail.com>
2019-10-21 20:39:22 +0200

    matching

--- Matching/image_matcher_eval.py
+++ Matching/image_matcher_eval.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import time
+import math
 import numpy as np
 import image_matcher as im
 from tabulate import tabulate
@@ -8,10 +9,15 @@ import dart_board
 
 numberOfKeypoint = 500
 numberOfCirclePointPerSector = 3
+max_correct_radius = 5
 
 
 def getRunTime(start, end):
     return str(end - start)
+
+
+def calcDistance(point1, point2):
+    return math.sqrt((point2[0] - point1[0]) * (point2[0] - point1[0]) + (point2[1] - point1[1]) * (point2[1] - point1[1]))
 
 
 def LoadPoints(filename):
@@ -36,29 +42,83 @@ def Project(points, homography_matrix):
     return newPoints
 
 
-def evaluate(matches, kp_ref, kp_perspective):
-    # TODO
-    pass
+def getMatchesPointWithHomography(kp_ref, matches, homography_matrix):
+    points = [kp_ref[match[0].queryIdx].pt for match in matches]
+    return Project(points, homography_matrix)
+
+
+def evaluate(matches, kp_ref, kp_perspective, truth_points, outputFolderName):
+    goodMatches = []
+    badMatches = []
+    points = [kp_perspective[match[0].trainIdx].pt for match in matches]
+
+    for i in range(0, len(points)):
+        d = calcDistance(points[i], truth_points[i])
+        if d <= max_correct_radius:
+            goodMatches.append(i)
+        else:
+            badMatches.append(i)
+
+    drawInlierOutlierPoints(img_perspective, points, truth_points, goodMatches, badMatches, outputFolderName)
+
+    return len(goodMatches), len(badMatches)
+
+
+def drawInlierOutlierPoints(img, points, truth_points, inliers, outliers, outputFolderName, isGray=True):
+    if isGray:
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+    directory = "output/" + outputFolderName
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    for i in outliers[:20]:
+        bad_point = (int(points[i][0]), int(points[i][1]))
+        good_point = (int(truth_points[i][0]), int(truth_points[i][1]))
+        img = cv2.circle(img, bad_point, 3, (0, 0, 255), thickness=-1)
+        img = cv2.circle(img, good_point, 3, (0, 255, 255), thickness=-1)
+        img = cv2.line(img, bad_point, good_point, (255, 0, 0), thickness=2)
+
+    for i in inliers:
+        img = cv2.circle(img, (int(points[i][0]), int(points[i][1])), 3, (0, 255, 0), thickness=-1)
+
+    cv2.imwrite(directory + '/' + name_perspective + '.png', img)
+
+
+def drawPoints(img, points, name, color=(0, 255, 0), isGray=True):
+    if isGray:
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+    for point in points:
+        img = cv2.circle(img, (int(point[0]), int(point[1])), 3, color, thickness=-1)
+
+    cv2.imshow(name, img)
 
 
 if __name__ == '__main__':
-    name_ref = '../images/darts1_1.jpg'
-    name_perspective = '../images/darts1_2.jpg'
+    name_ref = "darts1_2"
+    path_ref = '../images/' + name_ref + '.jpg'
+    name_perspective = "darts2_2"
+    path_perspective = '../images/' + name_perspective + '.jpg'
+    outputName = "cvBF_noprefilter_nocheck_nohamming_noratio"
     crossCheck = False
 
-    img_ref = cv2.imread(name_ref)
+    img_ref = cv2.imread(path_ref)
     img_ref = cv2.resize(img_ref, None, fx=0.25, fy=0.25, interpolation=cv2.INTER_CUBIC)
     img_ref = cv2.cvtColor(img_ref, cv2.COLOR_BGR2GRAY)
 
-    img_perspective = cv2.imread(name_perspective)
+    img_perspective = cv2.imread(path_perspective)
     img_perspective = cv2.resize(img_perspective, None, fx=0.25, fy=0.25, interpolation=cv2.INTER_CUBIC)
     img_perspective = cv2.cvtColor(img_perspective, cv2.COLOR_BGR2GRAY)
 
-    click_point_ref = LoadPoints(os.path.splitext(name_ref)[0])
-    click_point_perspective = LoadPoints(os.path.splitext(name_perspective)[0])
+    click_point_ref = LoadPoints(os.path.splitext(path_ref)[0] + '.click')
+    click_point_perspective = LoadPoints(os.path.splitext(path_perspective)[0] + '.click')
 
     refPoints, circlePoints = dart_board.generateDartBoardEdgePoints(numberOfCirclePointPerSector)
-    # TODO homográfiák számolása
+
+    homography_matrix_ref, _ = cv2.findHomography(np.array(refPoints), np.array(click_point_ref))
+    homography_matrix_perspective, _ = cv2.findHomography(np.array(refPoints), np.array(click_point_perspective))
+    homography_matrix_ground_truth = np.dot(homography_matrix_perspective, np.linalg.inv(homography_matrix_ref))
 
     # Initiate ORB
     start = time.time()
@@ -66,7 +126,6 @@ if __name__ == '__main__':
     kp_ref, des_ref = orb.detectAndCompute(img_ref, None)
     kp_perspective, des_perspective = orb.detectAndCompute(img_perspective, None)
     end = time.time()
-
     detectRunTime = getRunTime(start, end)
 
     # index_ref, index_perspective = \
@@ -77,12 +136,15 @@ if __name__ == '__main__':
     end = time.time()
     matchingRunTime = getRunTime(start, end)
 
+    truth_points = getMatchesPointWithHomography(kp_ref, matches, homography_matrix_ground_truth)
+    numberOfInliers, numberOfOutliers = evaluate(matches, kp_ref, kp_perspective, truth_points, outputName)
+
+    foundMatches = len(matches)
+
     mydata = [(
-        name_ref, "-", str(numberOfKeypoint), "-", name_perspective, str(numberOfKeypoint), detectRunTime,
-        "cvBF_noprefilter_nocheck_nohamming_noratio",
-        crossCheck, "-",
-        "-", "-", "-",
-        "-", "-", matchingRunTime)]
+        path_ref, "-", str(numberOfKeypoint), "-", path_perspective, str(numberOfKeypoint), detectRunTime,
+        outputName, crossCheck, "-", "-", foundMatches, str(foundMatches / numberOfKeypoint * 100),
+        numberOfInliers, str(numberOfInliers / foundMatches * 100), matchingRunTime)]
 
     headers = ["Referenciakép neve", "Előszűrés küszöbértéke", "Detektált pontok száma", "Szűrt pontok száma",
                "Perspektív kép neve", "Detektált pontok száma perspektív képen", "Detektálás futási ideje (ms)",
@@ -91,3 +153,5 @@ if __name__ == '__main__':
                "Párkeresés futási ideje (ms)"]
 
     print(tabulate(mydata, headers=headers))
+
+    cv2.waitKey(0)
