1ff55532bbfb43a1b612cf56d4779fd81124a1dc
GEOV\jonathan.ozsvath <ozsvathjonathan@gmail.com>
2019-11-05 16:15:54 +0100

    Nyíl kijelölés

--- Arrow/drawArrow.py
+++ Arrow/drawArrow.py
@@ -1,50 +1,68 @@
 import cv2
 import math
 import numpy as np
+import matplotlib.pyplot as plt
+
+isLButtonDown = False
+isRButtonDown = False
+isArrow = True
 
 
 def calcDistance(point1, point2):
-    return math.sqrt((point2[0] - point1[0]) * (point2[0] - point1[0]) + (point2[1] - point1[1]) * (point2[1] - point1[1]))
+    return math.sqrt(
+        (point2[0] - point1[0]) * (point2[0] - point1[0]) + (point2[1] - point1[1]) * (point2[1] - point1[1]))
 
 
-def showImageWithClicks(mouseClicks, radius=3, color=(0, 255, 0)):
+def showImageWithClicks(radius=3, color=(0, 255, 0)):
     imgCopy = img.copy()
-    width, height = imgCopy.shape[1], imgCopy.shape[0]
-    img_mask = np.zeros((height, width), dtype=np.uint8)
+    for point in arrow:
+        cv2.circle(imgCopy, point, radius, color, thickness=-1)
 
-    if len(mouseClicks) < 3:
-        for x, y in mouseClicks:
-            cv2.circle(imgCopy, (x, y), radius, color, thickness=-1)
-    else:
-        mouseClicks = np.array(mouseClicks)
-        imgCopy = cv2.fillPoly(imgCopy, [mouseClicks], color)
-        img_mask = cv2.fillPoly(img_mask, [mouseClicks], 255)
-
-        # arrow.clear()
-        # for y in range(0, height):
-        #     for x in range(0, width):
-        #         if img_mask[y, x] == 255:
-        #             arrow.append((x, y))
+    for point in notArrow:
+        cv2.circle(imgCopy, point, radius, (0, 0, 255), thickness=-1)
 
     cv2.imshow(name, imgCopy)
-    cv2.imshow("mask", img_mask)
 
 
 def setMouseCallback(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONUP:
-        mouseClicks.append([x, y])
-        showImageWithClicks(mouseClicks)
+    global isLButtonDown, isRButtonDown, isArrow
 
-    elif event == cv2.EVENT_RBUTTONUP:
-        for click in mouseClicks:
-            if calcDistance([x, y], click) < 10:
-                mouseClicks.remove(click)
-                showImageWithClicks(mouseClicks)
+    if event == cv2.EVENT_LBUTTONDOWN:
+        isLButtonDown = True
+
+    if event == cv2.EVENT_LBUTTONUP:
+        isLButtonDown = False
+
+    if isLButtonDown:
+        if isArrow:
+            arrow.add((x, y))
+            showImageWithClicks(radius=1)
+        else:
+            notArrow.add((x,y))
+            showImageWithClicks(radius=1)
+
+    if event == cv2.EVENT_RBUTTONDOWN:
+        isRButtonDown = True
+
+    if event == cv2.EVENT_RBUTTONUP:
+        isRButtonDown = False
+
+    if isRButtonDown:
+        if isArrow:
+            for point in arrow.copy():
+                if calcDistance((x, y), point) < 5:
+                    arrow.remove(point)
+                    showImageWithClicks(radius=1)
+        else:
+            for point in notArrow.copy():
+                if calcDistance((x, y), point) < 5:
+                    notArrow.remove(point)
+                    showImageWithClicks(radius=1)
 
 
 if __name__ == '__main__':
-    arrow = []
-    notArrow = []
+    arrow = set()
+    notArrow = set()
     mouseClicks = []
 
     name = "darts_with_arrow"
@@ -53,11 +71,37 @@ if __name__ == '__main__':
     img = cv2.imread(path)
     img = cv2.resize(img, None, fx=0.25, fy=0.25, interpolation=cv2.INTER_CUBIC)
 
+    img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+
     cv2.imshow(name, img)
 
     cv2.setMouseCallback(name, setMouseCallback)
+    while cv2.getWindowProperty(name, 0) >= 0:
 
-    k = cv2.waitKey(0)
+        k = cv2.waitKey(0)
 
-    if k == 27:
-        cv2.destroyAllWindows()
+        if k == 27:
+            cv2.destroyAllWindows()
+        elif k == 13:
+            isArrow = not isArrow
+            print(isArrow)
+
+    u_arrow = [img_yuv[y, x][1] for x, y in arrow]
+    v_arrow = [img_yuv[y, x][2] for x, y in arrow]
+
+    u_notArrow = [img_yuv[y, x][1] for x, y in notArrow]
+    v_notArrow = [img_yuv[y, x][2] for x, y in notArrow]
+
+    # plt.plot(u_arrow, v_arrow, 'gx')
+    # plt.plot(u_notArrow, v_notArrow, 'rx')
+    # plt.xlabel('U')
+    # plt.ylabel('V')
+
+    plt.plot(v_arrow, u_arrow, 'gx')
+    plt.plot(v_notArrow, u_notArrow, 'rx')
+    plt.xlabel('V')
+    plt.ylabel('U')
+
+# -(x - 150)² + 125 - y
+
+    plt.show()
