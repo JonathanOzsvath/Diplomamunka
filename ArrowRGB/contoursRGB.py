6656e0e1c47befe3cdd61f24eddf05b4cd8644e5
JonathanOzsvath <ozsvathjonathan@gmail.com>
2019-12-02 00:29:34 +0100

    kontúr

--- ArrowRGB/contoursRGB.py
+++ ArrowRGB/contoursRGB.py
@@ -13,29 +13,27 @@ if __name__ == '__main__':
     # img1 = cv2.Sobel(img1, cv2.CV_8U, 1, 1, ksize=11)
     # img1 = cv2.GaussianBlur(img1, (3, 3), 2)
     # img1 = cv2.Laplacian(img1, cv2.CV_8U)
-    img1 = cv2.medianBlur(img1, 11)
-    img1 = cv2.Canny(img1, 30, 200)
-    cv2.imshow('blur', img1)
+    # img1 = cv2.medianBlur(img1, 11)
+    # img1 = cv2.Canny(img1, 30, 200)
+    # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9, 9))
+    # dilated = cv2.dilate(img1, kernel)
+    # cv2.imshow('blur', img1)
 
-    contours, hierarchy = cv2.findContours(img1, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+    img2 = img1.copy()
+    img2 = cv2.cvtColor(img2, cv2.COLOR_GRAY2BGR)
+
+    contours, hierarchy = cv2.findContours(img1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # for c in contours:
+    #     cv2.drawContours(img2, [c], 0, (0, 0, 255), 3)
+    #     cv2.imshow("contours", img2)
+    #     cv2.waitKey(0)
 
     for c in contours:
-        # find minimum area
-        rect = cv2.minAreaRect(c)
-        # calculate coordinates of the minimum area rectangle
-        box = cv2.boxPoints(rect)
-        # normalize coordinates to integers
-        box = np.int0(box)
-        # draw contours
-        # cv2.drawContours(img, [box], 0, (0, 0, 255), 3)
-        #
-        # cv2.drawContours(img, contours, -1, (255, 0, 0), 1)
-        # cv2.imshow("contours", img)
+        area = cv2.contourArea(c)
 
-        epsilon = 0.02 * cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, epsilon, True)
-        hull = cv2.convexHull(c)
-        cv2.drawContours(img, hull, 0, (0, 0, 255), 3)
-        cv2.imshow("contours", img)
+        if  1000 < area < 1500:
+            hull = cv2.convexHull(c)
+            cv2.drawContours(img2, [hull], 0, (0, 0, 255), 1)
+            cv2.imshow("contours", img2)
 
     cv2.waitKey(0)
